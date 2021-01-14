23d0c893ed0ba5c1d67e2f4f19e1b7f7a4d5966d
Joas Schilling <coding@schilljs.com>
2021-01-14 17:14:58 +0100

    Trigger API request to delete a chat message when the button is pressed
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/store/messagesStore.js
+++ src/store/messagesStore.js
@@ -20,6 +20,7 @@
  *
  */
 import Vue from 'vue'
+import { deleteMessage } from '../services/messagesService'
 
 const state = {
 	/**
@@ -207,6 +208,7 @@ const actions = {
 	 */
 	deleteMessage(context, message) {
 		context.commit('deleteMessage', message)
+		deleteMessage(message)
 	},
 
 	/**
